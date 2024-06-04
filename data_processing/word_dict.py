9f068711bdce6b0ed698f5b1d7df4ed78b0ce662
hiiragi6666 <152354604+hiiragi6666@users.noreply.github.com>
2024-06-04 20:07:07 +0800

    Update word_dict.py

--- data_processing/word_dict.py
+++ data_processing/word_dict.py
@@ -37,6 +37,21 @@ def vocab_processing(filepath1, filepath2, save_path):
         f.write(str(word_set))
 
 
+# 获取两个文本数据集中出现的单词的集合，
+# 并且仅返回在第二个数据集中出现过而未在第一个数据集中出现过的单词的集合
+def final_vocab_processing(filepath1, filepath2, save_path):
+    with open(filepath1, 'r') as f:
+        total_data1 = set(eval(f.read()))
+
+    total_data2 = load_pickle(filepath2)
+    word_vocab = get_vocab(total_data1, total_data2)
+
+    new_words = word_vocab - total_data1
+
+    with open(save_path, 'w') as f:
+        f.write(str(new_words))
+
+
 if __name__ == "__main__":
     python_hnn = 'C:/Users/Lenovo/Desktop/data_processing/data/python_hnn_data_teacher.txt'
     python_staqc = 'C:/Users/Lenovo/Desktop/data_processing/data/staqc/python_staqc_data.txt'
@@ -49,5 +64,4 @@ if __name__ == "__main__":
     new_sql_staqc = 'C:/Users/Lenovo/Desktop/data_processing/ulabel_data/staqc/sql_staqc_unlabled_data.txt'
     new_sql_large = 'C:/Users/Lenovo/Desktop/data_processing/ulabel_data/large_corpus/multiple/sql_large_multiple_unlable.txt'
     large_word_dict_sql = 'C:/Users/Lenovo/Desktop/data_processing/ulabel_data/sql_word_dict.txt'
-
     final_vocab_processing(sql_word_dict, new_sql_large, large_word_dict_sql)
